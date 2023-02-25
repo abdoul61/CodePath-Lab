ccfdcc3545f0d81b553669b4697301809de94eff
Abdoul <azizamine61@yahoo.fr>
2023-02-25 18:29:35 -0500

    CodePath Lab1; react Calendear

---
+++ timetabled/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Calendar from './components/Calendar';
+
+const App = () => {
+
+  return (
+    <div className="App">
+		<h1>Title: Time table for the week!🚀</h1>
+		<h2>Welcome to New Jersey check this calendear to get to know the city</h2>
+			<Calendar/>
+    </div>
+  )
+}
+
+export default App
