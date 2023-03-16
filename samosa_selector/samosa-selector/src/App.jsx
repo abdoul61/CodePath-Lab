3cd790d01608f6d2198b54ed8238b8922bd413ad
Abdoul <azizamine61@yahoo.fr>
2023-03-15 20:40:19 -0400

    added week 2 lab

---
+++ samosa_selector/samosa-selector/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+			<div className="header">
+				<h1>
+				samosa selector	
+				</h1>
+				<h2>
+					Count:0
+				</h2>
+				<img src="https://food.fnr.sndimg.com/content/dam/images/food/fullset/2021/12/09/0/DV3501_samosa-curry-fried-chicken-salt-lake-city-2_s4x3.jpg.rend.hgtvcom.616.462.suffix/1639061347834.jpeg" className="samosa"/>
+			</div>
+    </div>
+  )
+}
+
+export default App
